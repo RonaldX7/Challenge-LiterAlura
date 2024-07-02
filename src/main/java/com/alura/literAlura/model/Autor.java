92ce7ed12c0de1aa63b08156bbb3321c42281945
RonaldX7 <ronaldjair0705@gmail.com>
2024-07-02 15:54:02 -0500

    nuevos cambios

--- src/main/java/com/alura/literAlura/model/Autor.java
+++ src/main/java/com/alura/literAlura/model/Autor.java
@@ -2,11 +2,19 @@ package com.alura.literAlura.model;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 @Table(name = "autores")
 public class Autor {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String nombre;
+    private String fechaNacimiento;
+    private String fechaFallecimiento;
+
+    @OneToMany(mappedBy = "libro", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Libro> libro;
 }
--- src/main/java/com/alura/literAlura/model/Libro.java
+++ src/main/java/com/alura/literAlura/model/Libro.java
@@ -2,11 +2,53 @@ package com.alura.literAlura.model;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 @Table(name = "libros")
 public class Libro {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String titulo;
+    private String idiomas;
+    private Integer numeroDeDescargas;
+
+    @ManyToOne
+    private Autor autores;
+
+    public Libro(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getIdiomas() {
+        return idiomas;
+    }
+
+    public void setIdiomas(String idiomas) {
+        this.idiomas = idiomas;
+    }
+
+    public Integer getNumeroDeDescargas() {
+        return numeroDeDescargas;
+    }
+
+    public void setNumeroDeDescargas(Integer numeroDeDescargas) {
+        this.numeroDeDescargas = numeroDeDescargas;
+    }
+
 }
